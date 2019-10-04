8ca242aab27c9eade30b0e396c8e8a066d830507
EvertonGavioli <vertopge@hotmail.com>
2019-10-04 16:11:35 -0300

    tools model and controller

---
+++ src/controllers/toolsController.js
@@ -0,0 +1,13 @@
+const Tools = require("../models/tools");
+
+//GET - list all registered tools
+exports.tools_list = async function(req, res) {};
+
+//GET - filtering tools using a tag search
+exports.tools_tag_filter = async function(req, res) {};
+
+//POST - Create a new Tool
+exports.tools_create = async function(req, res) {};
+
+//DELETE - Delete a tool by id
+exports.tools_delete = async function(req, res) {};
